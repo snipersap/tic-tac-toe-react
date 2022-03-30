8b05d1ed561403eff696c337f83293b36f73af65
snipersap <snipersap@gmail.com>
2022-03-31 00:13:51 +0200

    early return on win or filled square

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ import './index.css';
 
     handleClick(i) {
         const squaresSlice = this.state.squares.slice();
+        if(calculateWinner(squaresSlice) || squaresSlice[i]){
+          return; 
+        }
         squaresSlice[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
           squares:squaresSlice,
@@ -98,10 +101,10 @@ import './index.css';
       [0,4,8],
       [2,4,6],
     ];
-    for(let i = 0; i > lines.length; i++) {
+    for(let i = 0; i < lines.length; i++) {
       const[a, b, c] = lines[i];
       if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return squares[a]; 
       }
     }
     return null;
